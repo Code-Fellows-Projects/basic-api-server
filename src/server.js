4facb46d5c6bcc765e72e0924c0fa811f59356e7
Stacy Burris <stacy1burris@gmail.com>
2020-12-09 16:06:40 -0800

    basic server

---
+++ src/server.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+///////////////middleware//////////////////
+const logger = require('./middleware/logger');
+const notFoundHandler = require('./error-handlers/404');
+const serverError = require('./error-handlers/500');
+const clothesRoutes = require('./routes/clothes');
+
+////middleware
+app.use(express.json());
+app.use(logger);
+app.use(clothesRoutes);
+
+
+app.get('/tester', testerCallBackHandler);
+
+function testerCallBackHandler(req, res, next) {
+  res.status(200).send('The world is alive!');
+}
+
+
+
+
+
+app.use('*', notFoundHandler);
+app.use(serverError);
+
+
+
+module.exports = {
+  server: app,
+  start: port => {
+    if (!port) { throw new Error('cannot find port'); }
+    app.listen(port, () => {
+      console.log(`server up on ${port}`);
+    });
+  },
+};
